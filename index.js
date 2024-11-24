15162d4af36d7cd3ef4193722d586a6c0c718fed
philles85 <philster04@outlook.com>
2024-11-24 19:13:28 +0100

    Fler If till invalid namn & class.
    Här lägger ja till  två till if satser, en till om det skulle vara invalid, då lägger jag den utanför med en ny variabel "findmatch" som är där för att tyda på att det är falskt eller sant, funkade inte utan den. La även till en class till ena if satsen så rutan blir en annan färg med rätt namn,

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ function cityAsk(tag) {
         innerCity.textContent = element.name;
         if (tag === element.name) {
             h2.textContent = tag + " " + "(" + element.country + ")";
+            findMatch = true;
+        }
+        if (insert == element.name) {
+            innerCity.classList.add("target");
         }
     }
+    if (!findMatch) {
+        h2.textContent = insert + " " + "finns inte i databasen";
+    }
+
 }
 // Recommended: constants with references to existing HTML-elements
 const Main = document.querySelector("main");
@@ -16,6 +24,7 @@ const table = document.getElementById("table");
 const city = document.getElementById("cities");
 const insert = prompt("Insert city here!");
 const h2 = document.querySelector("h2");
+let findMatch = false;
 
 // Recommended: Ask for the city name and then the rest of the code
 
