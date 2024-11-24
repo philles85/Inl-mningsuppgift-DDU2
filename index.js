de86d901ef004d51e28534356b4159784ee2bd6a
philles85 <philster04@outlook.com>
2024-11-24 18:39:29 +0100

    Deklarerar en funktion till CityBox
    Här deklarerar jag en funktion med parametern tag, detta ska i sin tur vara argumentet för prompt. Här loopar jag igenom arrayn av cities, och skapar en div för varje stad namn rutorna. Jag lägger till stad namnen i dessa rutor med textcontent.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Recommended: All functions declared here
-
+function cityAsk(tag) {
+    for (let element of cities) {
+        let innerCity = document.createElement("div");
+        innerCity.classList.add("cityBox");
+        city.appendChild(innerCity);
+        innerCity.textContent = element.name;
+    }
+}
 // Recommended: constants with references to existing HTML-elements
 
+
 // Recommended: Ask for the city name and then the rest of the code
+
+
+
+
+
 
