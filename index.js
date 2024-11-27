453dfd4590510b8f6e1b74b9de96d35b15f88cfa
philles85 <philster04@outlook.com>
2024-11-27 06:11:50 +0100

    Två nya funktioner till grid
    Här har jag lagt till två nya funktioner för grid tabellen, där jag har till ena halvan och till den andra halvan, sedan har jag anropat de med en variabel i for loopen till griden.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Recommended: All functions declared here
-function cityAsk() {
+function createAllCityBoxes() {
     for (let element of cities) {
         let innerCity = document.createElement("div");
         innerCity.classList.add("cityBox");
@@ -8,16 +8,39 @@ function cityAsk() {
         if (insert == element.name) {
             h2.textContent = insert + " " + "(" + element.country + ")";
             findMatch = true;
-        }
-        if (insert == element.name) {
             innerCity.classList.add("target");
+            document.title = `${insert}`
         }
+
     }
+
     if (!findMatch) {
         h2.textContent = insert + " " + "finns inte i databasen";
     }
 }
 
+function firstHalf(i, j) {
+    let cel = document.createElement("div");
+    cel.classList.add("cell");
+    table.appendChild(cel);
+    cel.style.gridArea = `${i + 2} / ${j + 2}`;
+    if (j % 2 == 0) {
+        cel.classList.add("even_col");
+    }
+    return cel;
+}
+
+function secondHalf(j, i, f) {
+    let cel2 = document.createElement("div");
+    cel2.classList.add("cell");
+    table.appendChild(cel2);
+    cel2.textContent = Math.round(distances[f].distance / 10);
+    cel2.style.gridArea = `${j + 2} / ${i + 2}`;
+    if (i % 2 == 0) {
+        cel2.classList.add("even_col");
+    }
+    return cel2;
+}
 
 
 // Recommended: constants with references to existing HTML-elements
@@ -29,7 +52,10 @@ const h2 = document.querySelector("h2");
 let findMatch = false;
 
 // Recommended: Ask for the city name and then the rest of the code
-cityAsk();
+
+
+createAllCityBoxes();
+
 
 
 for (let k = 0; k < cities.length; k++) {
@@ -53,37 +79,23 @@ for (let p = 0; p <= 38; p++) {
     cel.textContent = p;
     cel.style.gridRow = "1";
     cel.style.gridColumn = p + 2;
-
 }
 
 
 
-for (let i = 1; i <= 38; i++) {
+for (let i = 0; i <= 38; i++) {
     let inner = i;
-    for (let j = 0; j < inner; j++) {
-        for (let f = 0; f < distances.length; f++) {
-            if (distances[f].city1 == i && distances[f].city2 == j) {
-                let cel = document.createElement("div");
-                cel.classList.add("cell");
-                table.appendChild(cel);
-                cel.textContent = Math.round(distances[f].distance / 10);
-                cel.style.gridArea = `${i + 2} / ${j + 2}`;
-            }
+    for (let j = 0; j <= inner; j++) {
+        let cel = firstHalf(i, j);
+        if (i == j) {
+            cel.textContent = " ";
         }
-    }
-}
-
-
-for (let i = 1; i <= 38; i++) {
-    let inner = i;
-    for (let j = 0; j < inner; j++) {
-        for (let f = 0; f < distances.length; f++) {
-            if (distances[f].city1 == i && distances[f].city2 == j) {
-                let cel = document.createElement("div");
-                cel.classList.add("cell");
-                table.appendChild(cel);
-                cel.textContent = Math.round(distances[f].distance / 10);
-                cel.style.gridArea = `${j + 2} / ${i + 2}`;
+        else {
+            for (let f = 0; f < distances.length; f++) {
+                if (distances[f].city1 == i && distances[f].city2 == j) {
+                    cel.textContent = Math.round(distances[f].distance / 10);
+                    let cel2 = secondHalf(j, i, f);
+                }
             }
         }
     }
